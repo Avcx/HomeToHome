22049335a5410fbaddf7d0e072e45ed58f812496
Tonio Adams <avcx12@gmail.com>
2017-04-03 19:42:21 -0600

    Fixed mobile and desktop layouts

--- menu.js
+++ menu.js
@@ -1,43 +1,13 @@
-const list = document.querySelector('.nav-container');
-const button = document.querySelector('.menu-button');
-const header = document.getElementsByClassName('head-wrap')[0];
-const arrow = document.getElementById('indicator');
-const firstContainer = document.getElementById('first-container');
-let lastScroll = 0;
-let isNavOpen = false;
 
-function toggleNav() {
-  console.log('toggleNav');
-  list.classList.toggle('open');
-  if (isNavOpen) {
-    isNavOpen = false;
-    document.body.style.overflow = 'auto';
-  } else {
-    isNavOpen = true;
-    document.body.style.overflow = 'hidden';
-  }
-}
-window.onload = function() {
-window.addEventListener('scroll', (event) => {
-  let currentScroll = window.pageYOffset || document.scrollTop || 0;
-  console.log(`currentScroll: ${currentScroll}`);
-  console.log(`Last Scroll: ${lastScroll}`);
-  if (currentScroll > lastScroll) {
-    console.log('scrolled down');
-    if (isNavOpen) {
-      toggleNav();
-    }
-    header.style.transform = 'translateY(-600px)';
-  } else {
-    console.log('scrolled up');
-    header.style.transform = 'translateY(0)';
-  }
-  lastScroll = currentScroll;
-})
-}
 
-arrow.addEventListener('click', (event) => {
-  firstContainer.scrollIntoView();
-})
-
-button.onclick = toggleNav;
+let lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+       // downscroll code
+   } else {
+      // upscroll code
+   }
+   lastScrollTop = st;
+}, false);
