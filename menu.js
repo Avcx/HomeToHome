c9f11f452613dcb03993ded11bde376e58012d1c
Tonio Adams <avcx12@gmail.com>
2017-04-07 19:24:54 -0600

    Sidebar toggle functions added

--- menu.js
+++ menu.js
@@ -1,13 +1,9 @@
+const menuOpenButton = document.querySelector('.open-nav');
+const menuCloseButton = document.querySelector('.close-nav');
+const sideBar = document.querySelector('#menu');
 
-
-let lastScrollTop = 0;
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-   if (st > lastScrollTop){
-       // downscroll code
-   } else {
-      // upscroll code
-   }
-   lastScrollTop = st;
-}, false);
+menuOpenButton.onclick = toggleNav;
+menuCloseButton.onclick = toggleNav;
+function toggleNav() {
+    sideBar.classList.toggle('open');
+}
