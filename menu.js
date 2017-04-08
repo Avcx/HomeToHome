7e95f9501df3ada6fe918e21b130a1268b815acc
Tonio Adams <avcx12@gmail.com>
2017-04-07 20:29:33 -0600

    Header appears/disappears at certain scrolls.

--- menu.js
+++ menu.js
@@ -1,9 +1,24 @@
 const menuOpenButton = document.querySelector('.open-nav');
 const menuCloseButton = document.querySelector('.close-nav');
+const content = document.querySelector('#info')
 const sideBar = document.querySelector('#menu');
+const header = document.querySelector("#main-header");
 
 menuOpenButton.onclick = toggleNav;
 menuCloseButton.onclick = toggleNav;
+
+window.addEventListener('scroll', (event) => {
+    const headerHeight = header.clientHeight;
+    const scroll = window.scrollY;
+    const contentHeight = sideBar.scrollHeight - headerHeight;
+
+    if (scroll > contentHeight) {
+        header.classList.remove('hide');
+    } else if (scroll < contentHeight - (headerHeight * 2)) {
+        header.classList.add('hide');
+    }
+});
+
 function toggleNav() {
     sideBar.classList.toggle('open');
 }
